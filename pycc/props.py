1118a274ebb7a67c8e42306b4bc7edcb93f6d2e7
Zachary Windom <zww4855@gmail.com>
2024-08-29 09:34:17 -0400

    added new props file

--- pycc/props.py
+++ pycc/props.py
@@ -1,11 +1,10 @@
 import numpy as np
 from pyscf.data import nist
 
-def dipole_moment(pyscf_mol,pyscf_mf,rdm1):
+def dipole_moment(pyscf_mol,pyscf_mf,rdm1,spin_rdm1):
     mo_coeff = pyscf_mf.mo_coeff
     with pyscf_mol.with_common_orig((0,0,0)):
         ao_dip = pyscf_mol.intor_symmetric('int1e_r', comp=3)
-
 
     scf_dm=pyscf_mf.make_rdm1()
     scf_dip = np.einsum('xij,ji->x', ao_dip, scf_dm).real
@@ -17,11 +16,38 @@ def dipole_moment(pyscf_mol,pyscf_mf,rdm1):
     mo_dip_x= (mo_coeff.T@ao_dip_x)@mo_coeff
     mo_dip_y= (mo_coeff.T@ao_dip_y)@mo_coeff
     mo_dip_z= (mo_coeff.T@ao_dip_z)@mo_coeff
-
+############# MO spatial to MO spin
+#    Ca = Cb = np.asarray(pyscf_mf.mo_coeff)
+#
+#    print('mo fock:\n',np.diag((Ca.T@pyscf_mf.get_fock())@Ca))
+#    C = np.block([
+#             [      Ca           ,   np.zeros_like(Cb) ],
+#             [np.zeros_like(Ca)  ,          Cb         ]
+#            ])
+#
+#    CT = np.block([
+#             [      Ca.T           ,   np.zeros_like(Cb) ],
+#             [np.zeros_like(Ca)  ,          Cb.T        ]
+#            ])
+#    z_dm= np.block([
+#             [      ao_dip_z           ,   np.zeros_like(ao_dip_z) ],
+#             [np.zeros_like(ao_dip_z)  ,          ao_dip_z         ]
+#            ])
+#
+#    trans_z_dip=(CT@z_dm)@C
+#    print('diag z mo dip mom:\n',np.diag(trans_z_dip))
+#    eps_a = np.asarray(pyscf_mf.mo_energy)
+#    eps = np.append(eps_a,eps_a)
+#
+#    print('eps argsort:',eps.argsort())
+#    #trans_z_dip=trans_z_dip[:,eps.argsort()]
+#    #print('sorted diag z dip mom:\n',np.diag(trans_z_dip))
+#    corr_spin_z=np.einsum('ij,ji->', trans_z_dip, spin_rdm1).real 
+#   
+#############
     corr_dip_x=np.einsum('ij,ji->', mo_dip_x, rdm1).real
     corr_dip_y=np.einsum('ij,ji->', mo_dip_y, rdm1).real
     corr_dip_z=np.einsum('ij,ji->', mo_dip_z, rdm1).real
-
     corr_dip=np.asarray([corr_dip_x,corr_dip_y,corr_dip_z])
     #sys.exit()
     charges = pyscf_mol.atom_charges()
